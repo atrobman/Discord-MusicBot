82fef8bef1fe3dae143d466dd787bf117ac9c551
Andrew Trobman <atrobman@vt.edu>
2021-09-20 23:24:59 -0400

    Swapped old style .format to f-strings

--- cogs/music.py
+++ cogs/music.py
@@ -66,7 +66,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
         self.stream_url = data.get('url')
 
     def __str__(self):
-        return '**{0.title}** by **{0.uploader}**'.format(self)
+        return f'**{self.title}** by **{self.uploader}**'
 
     @classmethod
     async def create_source(cls, ctx: commands.Context, search: str, *, loop: asyncio.BaseEventLoop = None):
@@ -76,7 +76,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
         data = await loop.run_in_executor(None, partial)
 
         if data is None:
-            raise YTDLError('Couldn\'t find anything that matches `{}`'.format(search))
+            raise YTDLError(f'Couldn\'t find anything that matches `{search}`')
 
         if 'entries' not in data:
             process_info = data
@@ -88,14 +88,14 @@ class YTDLSource(discord.PCMVolumeTransformer):
                     break
 
             if process_info is None:
-                raise YTDLError('Couldn\'t find anything that matches `{}`'.format(search))
+                raise YTDLError(f'Couldn\'t find anything that matches `{search}`')
 
         webpage_url = process_info['webpage_url']
         partial = functools.partial(cls.ytdl.extract_info, webpage_url, download=False)
         processed_info = await loop.run_in_executor(None, partial)
 
         if processed_info is None:
-            raise YTDLError('Couldn\'t fetch `{}`'.format(webpage_url))
+            raise YTDLError(f'Couldn\'t fetch `{webpage_url}`')
 
         if 'entries' not in processed_info:
             info = processed_info
@@ -105,7 +105,7 @@ class YTDLSource(discord.PCMVolumeTransformer):
                 try:
                     info = processed_info['entries'].pop(0)
                 except IndexError:
-                    raise YTDLError('Couldn\'t retrieve any matches for `{}`'.format(webpage_url))
+                    raise YTDLError(f'Couldn\'t retrieve any matches for `{webpage_url}`')
 
         return cls(ctx, discord.FFmpegPCMAudio(info['url'], **cls.FFMPEG_OPTIONS), data=info)
 
@@ -117,13 +117,13 @@ class YTDLSource(discord.PCMVolumeTransformer):
 
         duration = []
         if days > 0:
-            duration.append('{} days'.format(days))
+            duration.append(f'{days} days')
         if hours > 0:
-            duration.append('{} hours'.format(hours))
+            duration.append(f'{hours} hours')
         if minutes > 0:
-            duration.append('{} minutes'.format(minutes))
+            duration.append(f'{minutes} minutes')
         if seconds > 0:
-            duration.append('{} seconds'.format(seconds))
+            duration.append(f'{seconds} seconds')
 
         return ', '.join(duration)
 
@@ -136,12 +136,12 @@ class Song:
 
     def create_embed(self):
         embed = (discord.Embed(title='Now playing',
-                               description='```css\n{0.source.title}\n```'.format(self),
+                               description=f'```css\n{self.source.title}\n```',
                                color=discord.Color.blurple())
                  .add_field(name='Duration', value=self.source.duration)
                  .add_field(name='Requested by', value=self.requester.mention)
-                 .add_field(name='Uploader', value='[{0.source.uploader}]({0.source.uploader_url})'.format(self))
-                 .add_field(name='URL', value='[Click]({0.source.url})'.format(self))
+                 .add_field(name='Uploader', value=f'[{self.source.uploader}]({self.source.uploader_url})')
+                 .add_field(name='URL', value=f'[Click]({self.source.url})')
                  .set_thumbnail(url=self.source.thumbnail))
 
         return embed
@@ -324,6 +324,7 @@ class Music(commands.Cog):
             return
 
         ctx.voice_state.skip()
+        await ctx.send(f"Skipped song")
 
     @commands.command(pass_context=True, name="queue", aliases=["q", "list", "songs", "playlist"])
     async def queue(self, ctx, page: int = 1):
@@ -379,7 +380,7 @@ class Music(commands.Cog):
             try:
                 source = await YTDLSource.create_source(ctx, search, loop=self.bot.loop)
             except YTDLError as e:
-                await ctx.send('An error occurred while processing this request: {}'.format(str(e)))
+                await ctx.send(f'An error occurred while processing this request: {e}')
             else:
                 song = Song(source)
                 await ctx.voice_state.queue.put(song)
